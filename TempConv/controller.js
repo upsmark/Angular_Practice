d804031244b3327ed11c4bcf2c95aadb74313bd9
Mark Upson <markaupson@gmail.com>
2017-05-23 12:04:07 -0400

    temp conv with template

--- TempConv/controller.js
+++ TempConv/controller.js
@@ -1,17 +1,21 @@
 var app = angular.module('myApp');
 
 app.controller('myCtrl', function($scope){
-  $scope.convert = [];
+  $scope.temperaturesC = [];
+  $scope.temperaturesF = [];
 
-  $scope.submit = function submit(temp){
+  $scope.submit = function(temp){
 
-    $scope.convert.push(temp * 9/5)+32;
+    $scope.temperaturesC.push((temp * 9/5)+32);
 
        }
 
-   $scope.submit1 = function submit(temp){
 
-    $scope.convert.push(temp - 32) * (5/9);
+
+   $scope.submit1 = function(temp){
+
+
+    $scope.temperaturesF.push((temp - 32) * (5/9));
 
     }
   });
--- TempConv/script.js
+++ TempConv/script.js
@@ -3,7 +3,15 @@ var app = angular.module('myApp', []);
 app.directive('convertDirective', function(){
         return{
            restrict: "E",
-           templateUrl: "converted.html",
+           template: "<h3>{{ temperature }}</h3>",
            replace: false
         };
    });
+
+   app.directive('convertDirectiveB', function(){
+           return{
+              restrict: "E",
+              template: "<h3>{{ temperature }}</h3>",
+              replace: false
+           };
+      });

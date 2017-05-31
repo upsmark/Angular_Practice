222571b581d7439317d0c6cd1ead7ae8c8ab5038
Mark Upson <markaupson@gmail.com>
2017-05-31 14:01:50 -0400

    mvp

--- poems/public/js/app.js
+++ poems/public/js/app.js
@@ -1,41 +1,60 @@
-(function(){
+
 
 var app = angular.module('poems', ['ngRoute']);
 
 app.config(function($routeProvider){
-  $routeProvider.when('/ocean',{
-    controller: "themesCtrl",
-    templateUrl: "views/ocean.html"
-  })
+  $routeProvider
 
+    .when('/ocean',{
+     controller: "oceanCtrl",
+     templateUrl: "views/ocean.html"
+    })
     .when('/moon', {
-      controller: "themesCtrl",
+      controller: "moonCtrl",
       templateUrl: "views/moon.html"
     })
-
     .when('/forest', {
-      controller: "themesCtrl",
+      controller: "forestCtrl",
       templateUrl: "views/forest.html"
     })
-
     .when('/city', {
-      controller: "themesCtrl",
+      controller: "cityCtrl",
       templateUrl: "views/city.html"
     })
-
     .when('/jungle', {
-      controller: "themesCtrl",
+      controller: "jungleCtrl",
       templateUrl: "views/jungle.html"
     })
-
     .when('/desert', {
-      controller: "themesCtrl",
+      controller: "desertCtrl",
       templateUrl: "views/desert.html"
     })
-
+    .when('/display-ocean', {
+      controller: 'oceanCtrl',
+      templateUrl: 'views/display-ocean.html'
+    })
+    .when('/display-city',{
+      controller: 'cityCtrl',
+      templateUrl: 'views/display-city.html'
+    })
+    .when('/display-desert',{
+      controller: 'desertCtrl',
+      templateUrl: 'views/display-desert.html'
+    })
+    .when('/display-forest',{
+      controller: 'forestCtrl',
+      templateUrl: 'views/display-forest.html'
+    })
+    .when('/display-jungle',{
+      controller: 'jungleCtrl',
+      templateUrl: 'views/display-jungle.html'
+    })
+    .when('/display-moon',{
+      controller: 'moonCtrl',
+      templateUrl: 'views/display-moon.html'
+    })
     .otherwise({
-      redirectTo: "/index"
+      redirectTo: "/home"
+    })
+    
     });
-}); //closes app.config
-
-})(); //closes IIFE
